1a6c26cc1757e66b51eb91256ddbc9e7cad4ae53
Pedro Galvão <taquiles@gmail.com>
2018-08-23 00:56:15 +0100

    App.js edited
     On branch master
     Changes to be committed:
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Para começar a andar, edit <code>src/App.js</code> and save to reload.
+          Para começar a andar, editar <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
