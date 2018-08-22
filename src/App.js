c685b27743660e13fdbb92a7eb8d2f30ae4ee1cb
Pedro Galvão <taquiles@gmail.com>
2018-08-22 17:20:10 +0100

    To start walking
     Changes to be committed:
    	modified:   .gitignore
    	modified:   .travis.yml
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Para cemeçar, edit <code>src/App.js</code> and save to reload.
+          Para cemeçar a andar, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
